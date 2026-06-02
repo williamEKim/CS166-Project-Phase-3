9d204a8b98c82c798d16b53851ffd685fe1b188e
William Kim <wkim107@xe-10.cs.ucr.edu>
2026-06-02 15:20:19 -0700

    updated the windows equivalent executable

--- Ebay_DB.bat
+++ Ebay_DB.bat
@@ -1,14 +1,27 @@
 @echo off
 setlocal
 
-set "DB_NAME=%USERNAME%_eBay_DB"
 set "SCRIPT_DIR=%~dp0"
+set "DB_NAME=%USERNAME%_eBay_DB"
 
-choice /c YN /m "Would you like to execute the Python Script? (Y/N)"
+call "%SCRIPT_DIR%cs166_server\psql.bat"
+
+echo.
+choice /c YN /m "Would you like to execute the Python Script?"
 
 if %ERRORLEVEL% == 1 (
     echo Executing the Python Script...
     python "%SCRIPT_DIR%backend\main.py" "%DB_NAME%" "%PGPORT%" "%USERNAME%"
 ) else (
     echo Skipping Python Script.
+)
+
+echo.
+choice /c YN /m "Would you like to stop your PostgreSQL Server?"
+
+if %ERRORLEVEL% == 1 (
+    echo Stopping the PostgreSQL Server...
+    call "%SCRIPT_DIR%cs166_server\stopPostgreDB.bat"
+) else (
+    echo Skipping Server Stop.
 )
---
+++ cs166_server/psql.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+set "CURR_DIR=%~dp0"
+set "DB_NAME=%USERNAME%_eBay_DB"
+
+call "%CURR_DIR%startPostgreSQL.bat"
+call "%CURR_DIR%createPostgreDB.bat"
+
+psql -p %PGPORT% -d %DB_NAME% -f "%CURR_DIR%create_tables.sql"
